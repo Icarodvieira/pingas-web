b2494ec2f6c154a92ce3907195e621fdf84b85b2
Icarodvieira <icarodvieira3@gmail.com>
2026-03-26 16:42:56 -0300

    chore: configurar domínio Cloudinary no next.config.js
    - Substitui images.domains (deprecated) por images.remotePatterns
    - Permite carregar imagens de res.cloudinary.com via Next.js Image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { domains: [] },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
